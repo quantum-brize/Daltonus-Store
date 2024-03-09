a428448fee9bc9cc4de9598ea16252c8fff5e0f4
rohan0420 <skrohan0420@gmail.com>
2024-03-09 15:01:04 +0530

    added toggleable theme

--- app/Controllers/Api/Product_Controller.php
+++ app/Controllers/Api/Product_Controller.php
@@ -16,25 +16,33 @@ class Product_Controller extends Api_Controller
     {
         $resp = [
             'status' => false,
-            'message' => 'Product_not_added',
-            'data'  => $data
+            'message' => 'Product not added',
+            'data' => $data
         ];
-        
 
+        if (empty($this->request->getPost('title'))) {
+            $resp['message'] = 'Your Product Has No Name';
+        } else if (empty($this->request->getPost('details'))) {
+            $resp['message'] = 'Please add Some Details About Your Product';
+        } else if (empty($this->request->getPost('price'))) {
+            $resp['message'] = 'Set The Price Of Your Product';
+        } else if (empty($this->request->getPost('categoryId'))) {
+            $resp['message'] = 'Set The Category Of Your Product';
+        }
 
 
 
 
         return $resp;
 
-        
+
     }
 
 
 
 
 
-    
+
 
 
     public function POST_add_product()
