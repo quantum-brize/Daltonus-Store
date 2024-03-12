93597be83e2886c589fd297729b12c24c54670d7
rohan0420 <skrohan0420@gmail.com>
2024-03-12 19:12:46 +0530

    added files

--- app/Views/admin/products.php
+++ app/Views/admin/products.php
@@ -400,9 +400,6 @@
 
                                 <div class="tab-pane" id="productnav-draft" role="tabpanel">
                                     <div class="py-4 text-center">
-                                        <lord-icon src="../../../msoeawqm-2.json" trigger="loop"
-                                            colors="primary:#405189,secondary:#0ab39c" style="width:72px;height:72px">
-                                        </lord-icon>
                                         <h5 class="mt-4">Sorry! No Result Found</h5>
                                     </div>
                                 </div>
