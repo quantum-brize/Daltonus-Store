163109070bfb9cf215283e70dde2de62972719da
rohan0420 <skrohan0420@gmail.com>
2024-03-13 19:37:42 +0530

    dataTables

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,13 +31,13 @@ $routes->get('/privacy-policy',                 'Frontend\Frontend_Controller::p
 $routes->get('/faq',                            'Frontend\Frontend_Controller::faq');
 $routes->get('/invoice',                        'Frontend\Order_Controller::invoice');
 $routes->get('/order/success',                  'Frontend\Order_Controller::order_success');
+$routes->get('/order/track',                    'Frontend\Order_Controller::track_order');
+$routes->get('/order/history',                  'Frontend\Order_Controller::order_history');
 $routes->get('/contact-us',                     'Frontend\Frontend_Controller::contact_us');
 $routes->get('/address',                        'Frontend\Frontend_Controller::address');
-$routes->get('/track/order',                    'Frontend\Order_Controller::track_order');
 $routes->get('/payment',                        'Frontend\Order_Controller::payment');
 $routes->get('/review',                         'Frontend\Product_Controller::review');
 $routes->get('/conformation',                   'Frontend\Order_Controller::conformation');
-$routes->get('/order/history',                  'Frontend\Order_Controller::order_history');
 $routes->get('/cart',                           'Frontend\Product_Controller::cart');
 $routes->get('/wishlist',                       'Frontend\Product_Controller::wishlist');
 
